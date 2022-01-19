78926526164c106b4415230333bab20f2d4ac06b
simonamarcu2 <supersimy16@gmail.com>
2022-01-19 13:41:16 +0000

    key sound working

--- ex.js
+++ ex.js
@@ -1,71 +1,55 @@
-const sound = document.getElementById(".sound");
-const button = document.getElementsByClassName(".key");
+// const sound = document.getElementById("sound");
+const button = document.getElementById("key");
 
 document.addEventListener("keypress", (event) => {
   if (event.key == "a") {
-    const keySound = document.getElementById("audio");
-    keySound.volume = 0.3;
-    keySound.play();
-  }
-});
-document.addEventListener("keypress", (event) => {
-  if (event.key == "a") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioA");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "s") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioS");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "d") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioD");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "f") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioF");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "g") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioG");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "h") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioH");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 document.addEventListener("keypress", (event) => {
   if (event.key == "j") {
-    const keySound = document.getElementById("audio");
+    const keySound = document.getElementById("audioJ");
     keySound.volume = 0.3;
     keySound.play();
   }
 });
 
-document.addEventListener("click", (event) => {
-  if (event.key == "a") {
-    const keySound = document.getElementsByClassName("key");
-    keySound.volume = 0.3;
-    keySound.play();
-  }
-})
 
-// function playSound(soundfile) {
-//   document.getElementsByClassName("key").innerHTML="<embed src=\""+soundfile+"\"hidden=\"true\"autostart=\"true\" loop=\"false\"/>;
-// }
+button.addEventListener("click", playSound);
