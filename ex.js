9ff00984b3820f1122a243b4e43909a027c6b6bd
simonamarcu2 <supersimy16@gmail.com>
2022-01-19 17:50:59 +0000

    grand finale in DO major

--- ex.js
+++ ex.js
@@ -1,5 +1,108 @@
-// const sound = document.getElementById("sound");
-const button = document.getElementById("key");
+
+//declare a variable and get element of audio by ID
+const soundA = document.getElementById('audioA');
+const buttonA = document.getElementById('buttonA');
+const soundS = document.getElementById('audioS');
+const buttonS = document.getElementById('buttonS');
+const soundD = document.getElementById('audioD');
+const buttonD = document.getElementById('buttonD');
+const soundF = document.getElementById('audioF');
+const buttonF = document.getElementById('buttonF');
+const soundG = document.getElementById('audioG');
+const buttonG = document.getElementById('buttonG');
+const soundH = document.getElementById('audioH');
+const buttonH = document.getElementById('buttonH');
+const soundJ = document.getElementById('audioJ');
+const buttonJ = document.getElementById('buttonJ');
+
+//Use the event listener method to target key and assign sound by keypress
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 65) {
+//     soundA.pause();
+//     soundA.currentTime = 0;
+//     soundA.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 83) {
+//     soundS.pause();
+//     soundS.currentTime = 0;
+//     soundS.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 68) {
+//     soundD.pause();
+//     soundD.currentTime = 0;
+//     soundD.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 70) {
+//     soundF.pause();
+//     soundF.currentTime = 0;
+//     soundF.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 71) {
+//     soundG.pause();
+//     soundG.currentTime = 0;
+//     soundG.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 72) {
+//     soundH.pause();
+//     soundH.currentTime = 0;
+//     soundH.play();
+//   }
+// });
+// document.addEventListener('keypress', (keyValue) => {
+//   if (keyValue.keyCode == 74) {
+//     soundJ.pause();
+//     soundJ.currentTime = 0;
+//     soundJ.play();
+//   }
+// });
+
+//use the event listner method to target element by click
+buttonA.addEventListener('click', () => {
+  soundA.pause();
+  soundA.currentTime = 0;
+  soundA.play();
+});
+buttonS.addEventListener('click', () => {
+  soundS.pause();
+  soundS.currentTime = 0;
+  soundS.play();
+});
+buttonD.addEventListener('click', () => {
+  soundD.pause();
+  soundD.currentTime = 0;
+  soundD.play();
+});
+buttonF.addEventListener('click', () => {
+  soundF.pause();
+  soundF.currentTime = 0;
+  soundF.play();
+});
+buttonG.addEventListener('click', () => {
+  soundG.pause();
+  soundG.currentTime = 0;
+  soundG.play();
+});
+buttonH.addEventListener('click', () => {
+  soundH.pause();
+  soundH.currentTime = 0;
+  soundH.play();
+});
+buttonJ.addEventListener('click', () => {
+  soundJ.pause();
+  soundJ.currentTime = 0;
+  soundJ.play();
+});
+
 
 document.addEventListener("keypress", (event) => {
   if (event.key == "a") {
